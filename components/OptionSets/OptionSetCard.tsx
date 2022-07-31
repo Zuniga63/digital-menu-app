8b9eec49de28a896261da1f84c9c66f68f6b60c7
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-07-31 15:46:00 -0500

    add collapse component in the body card

--- components/OptionSets/OptionSetCard.tsx
+++ components/OptionSets/OptionSetCard.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import { Button } from '@mantine/core';
+import React, { useState } from 'react';
 import { IOptionSet } from 'pages/admin/set-de-opciones';
-import { Trash } from 'tabler-icons-react';
+
+import { Button, Collapse } from '@mantine/core';
+import { Trash, Plus, Minus } from 'tabler-icons-react';
 import OptionItemCard from './OptionItemCard';
 
 interface Props {
@@ -10,19 +11,34 @@ interface Props {
   onDelete?(id: string): Promise<void>;
 }
 export default function OptionSetCard({ optionSet, deleteLoading, onDelete }: Props) {
+  //-------------------------------------------------------------------
+  // STATE
+  //-------------------------------------------------------------------
+  const [opened, setOpened] = useState(false);
+
+  //-------------------------------------------------------------------
+  // Methods
+  //-------------------------------------------------------------------
   const handledClick = (): void => {
     if (onDelete) onDelete(optionSet.id);
   };
   return (
     <div>
-      <header className="rounded-t-md bg-slate-800 py-3">
-        <h3 className="text-center text-lg font-bold uppercase tracking-widest text-white">{optionSet.name}</h3>
+      <header className="flex gap-x-2 rounded-t-md bg-slate-800 py-3 px-4">
+        <h3 className="flex-grow text-center text-lg font-bold uppercase tracking-widest text-white">
+          {optionSet.name}
+        </h3>
+        <button type="button" className="flex-shrink-0 grow-0 text-light" onClick={() => setOpened(!opened)}>
+          {!opened ? <Plus /> : <Minus />}
+        </button>
       </header>
-      <ul className="flex flex-col gap-y-4 border-x border-slate-800 border-opacity-20 bg-white bg-opacity-80 px-2 py-4 backdrop-blur">
-        {optionSet.items.map((item) => (
-          <OptionItemCard key={item.id} item={item} />
-        ))}
-      </ul>
+      <Collapse in={opened}>
+        <ul className="flex flex-col gap-y-4 border-x border-slate-800 border-opacity-20 bg-white bg-opacity-80 px-2 py-4 backdrop-blur">
+          {optionSet.items.map((item) => (
+            <OptionItemCard key={item.id} item={item} />
+          ))}
+        </ul>
+      </Collapse>
       <footer className="rounded-b-md bg-slate-800 py-4 px-6">
         <Button
           color="red"
