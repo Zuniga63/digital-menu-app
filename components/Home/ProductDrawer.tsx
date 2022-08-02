ac77794b6755d2c9258f85eed544354311d5a483
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-08-02 08:59:44 -0500

    add small fix in the option set list

--- components/Home/ProductDrawer.tsx
+++ components/Home/ProductDrawer.tsx
@@ -98,6 +98,7 @@ export default function ProductDrawer() {
           )}
           <div className="px-6 py-8">
             {product?.description && <p className="mb-4 text-lg text-light"> {product?.description} </p>}
+            {/* Price and Discount */}
             <div className="flex flex-col items-end">
               <p className="font-bold tracking-widest text-light">
                 {!!discountPercentage && <span className="text-sm">antes </span>}
@@ -121,7 +122,7 @@ export default function ProductDrawer() {
                   <header className="rounded-t bg-dark px-6 py-4">
                     <h3 className="text-xl font-bold tracking-wider text-yellow-400">{optionSet.title}</h3>
                   </header>
-                  <ul className="divide-y divide-slate-400 rounded-b border-x border-b border-dark bg-gradient-to-br from-slate-900 to-slate-400 py-6 px-2">
+                  <ul className="max-h-96 divide-y divide-slate-400 overflow-y-auto rounded-b border-x border-b border-dark bg-gradient-to-br from-slate-900 to-slate-400 py-6 px-2">
                     {optionSet.items.map(
                       (item) =>
                         item.published &&
@@ -135,6 +136,7 @@ export default function ProductDrawer() {
                                     width={item.optionSetItem.image.width}
                                     height={item.optionSetItem.image.height}
                                     layout="responsive"
+                                    loading="lazy"
                                   />
                                 )}
                               </figure>
