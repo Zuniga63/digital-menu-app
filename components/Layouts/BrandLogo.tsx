0e0c702472d645d124474b966022438a5a9d971e
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-08-07 14:10:41 -0500

    pass Href anchor

--- components/Layouts/BrandLogo.tsx
+++ components/Layouts/BrandLogo.tsx
@@ -7,8 +7,8 @@ interface IBrandLogoProps {
 
 export default function BrandLogo({ onClick }: IBrandLogoProps) {
   return (
-    <Link href="/">
-      <a href="" className="relative block w-12 overflow-hidden rounded-full" onClick={onClick}>
+    <Link href="/" passHref>
+      <a href="" className="relative block w-10 overflow-hidden" onClick={onClick}>
         <Image
           src="https://ui-avatars.com/api/?background=0D8ABC&color=fff&name=Digital+Menu"
           alt="Digital Menu"
