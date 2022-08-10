a65c29ff407cc68b8ce650e34b2d33fb8df7546d
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-08-10 13:19:12 -0500

    add scroll to drawer

--- components/Layouts/AppLayout/NavDrawer.tsx
+++ components/Layouts/AppLayout/NavDrawer.tsx
@@ -41,44 +41,42 @@ export default function NavDrawer({ categories }: Props) {
 
   return (
     <Drawer opened={menuIsOpen} onClose={toggle} padding={0} size={largeScreen ? 'md' : '100%'} withCloseButton={false}>
-      <>
+      <div className="relative h-full overflow-y-auto pb-40">
         <NavDrawerHeader />
-        <div>
-          <h2 className="mb-4 border-b-4 border-double border-gray-400 py-2 px-4 text-center text-xl uppercase italic">
-            Categorías
-          </h2>
-          <ul className="divide-y divide-gray-200">
-            {categories.map(
-              (category) =>
-                category.isEnabled &&
-                !!category.products.length && (
-                  <li key={category.id} className="block">
-                    <button type="button" onClick={() => goToCategory(category.id)} className="block w-full">
-                      <div className="flex items-center gap-3 p-4 text-dark">
-                        {category.image && (
-                          <figure
-                            className="relative aspect-square w-14 shrink-0 overflow-hidden rounded-full ring-4 ring-gray-400"
-                            role="presentation"
-                          >
-                            <Image src={category.image.url} alt={category.name} layout="fill" />
-                          </figure>
+        <h2 className="mb-4 border-b-4 border-double border-gray-400 py-2 px-4 text-center text-xl uppercase italic">
+          Categorías
+        </h2>
+        <ul className="divide-y divide-gray-200">
+          {categories.map(
+            (category) =>
+              category.isEnabled &&
+              !!category.products.length && (
+                <li key={category.id} className="block">
+                  <button type="button" onClick={() => goToCategory(category.id)} className="block w-full">
+                    <div className="flex items-center gap-3 p-4 text-dark">
+                      {category.image && (
+                        <figure
+                          className="relative aspect-square w-14 shrink-0 overflow-hidden rounded-full ring-4 ring-gray-400"
+                          role="presentation"
+                        >
+                          <Image src={category.image.url} alt={category.name} layout="fill" />
+                        </figure>
+                      )}
+                      <div className="flex-grow">
+                        <h3 className="font-hand text-xl font-black">{category.name}</h3>
+                        {category.description && (
+                          <p className="mt-2 font-sans text-xs font-medium tracking-wider text-gray-600 line-clamp-2">
+                            {category.description}
+                          </p>
                         )}
-                        <div className="flex-grow">
-                          <h3 className="font-hand text-xl font-black">{category.name}</h3>
-                          {category.description && (
-                            <p className="mt-2 font-sans text-xs font-medium tracking-wider text-gray-600 line-clamp-2">
-                              {category.description}
-                            </p>
-                          )}
-                        </div>
                       </div>
-                    </button>
-                  </li>
-                )
-            )}
-          </ul>
-        </div>
-      </>
+                    </div>
+                  </button>
+                </li>
+              )
+          )}
+        </ul>
+      </div>
     </Drawer>
   );
 }
