61ee9c9b075c21640a8df040b095c7ff3a6515bb
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-08-11 08:32:59 -0500

    update name of header

--- components/Layouts/BrandName.tsx
+++ components/Layouts/BrandName.tsx
@@ -7,8 +7,12 @@ interface IBrandNameProps {
 export default function BrandName({ onClick }: IBrandNameProps) {
   return (
     <Link href="/">
-      <a href="" className="m-0 block text-center font-display text-lg capitalize no-underline" onClick={onClick}>
-        Digital Menu
+      <a
+        href=""
+        className="m-0 block text-center font-display text-lg capitalize tracking-widest no-underline"
+        onClick={onClick}
+      >
+        SD4
       </a>
     </Link>
   );
